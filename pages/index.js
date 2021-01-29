6134f67025a44bd3a3d3b3f719c51cf55737b6ad
FelipeWeiss <felipeew19@gmail.com>
2021-01-28 21:38:50 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import styled from 'styled-components'
+import Head from 'next/head';
+import { useRouter } from 'next/router'; 
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
 const Title = styled.h1`
   font-size: 50px;
@@ -29,15 +34,39 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
       <QuizBackground backgroundImage = {db.bg} >
+        <Head>
+          <title>AluraQuiz - Modelo Base</title>
+        </Head>
         <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>The Legends of Zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum dolor sit amet...</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+              }}>
+              <input
+                onChange={function(infosDoEvento){
+                  // State
+                  // infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz aí seu nome para jogar :)"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar  {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
